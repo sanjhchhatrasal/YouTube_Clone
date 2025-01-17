b0ae74bc6b29dfef0205c3940906b6c8e101d050
sanjh chhatrasal <sanjhchhatrasal@gmail.com>
2025-01-17 13:31:02 +0530

    Watch

---
+++ src/Components/Sidevideo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidevideo = () => {
+  return (
+    <div>Sidevideo</div>
+  )
+}
+
+export default Sidevideo
--- src/Components/Watch.jsx
+++ src/Components/Watch.jsx
@@ -10,6 +10,8 @@ import axios from "axios";
 const Watch = () => {
   const sidebar = useSelector((store) => store.sidebar.isSidebarOpen);
 
+  const [videoData, setVideoData] = useState("")
+
   const [searchParams] = useSearchParams();
   const vidId = searchParams.get('v');
   console.log("vid id=", vidId)
@@ -17,8 +19,9 @@ const Watch = () => {
 
   const getYoutubeVideo = async() => {
     try{
-      const resData = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&${vidId}&key=${API_KEY}`)
+      const resData = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${vidId}&key=${API_KEY}`)
       console.log(resData)
+      setVideoData(resData?.data?.items[0])
     } catch(err){
       console.log(err)
     }
@@ -36,16 +39,16 @@ const Watch = () => {
             <div className="h-[65vh] w-[55vw] rounded-xl bg-zinc-800 overflow-hidden">
                <iframe
             className="h-full w-full object-cover"
-            src={`https://www.youtube.com/embed/${vidId}?si=Rs1s0VlC8nqqqcpG`}
+            src={`https://www.youtube.com/embed/${vidId}?autoplay=1`}
             title="YouTube video player"
-            frameborder="0"
+            // frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
-            allowfullscreen
+            referrepolicy="strict-origin-when-cross-origin"
+            allowFullScreen
           ></iframe>
             </div>
-            <h1 className="text-[1.3rem] font-semibold">
-              Build & Deploy YouTube Clone | ReactJS + Redux + Tailwind
+            <h1 className="text-[1.3rem] pt-3 font-semibold">
+              {videoData?.snippet?.title}
             </h1>
             <div className="flex items-start gap-2 pt-3 w-[55vw]">
               <div className="flex gap-2 w-[18vw]">
@@ -53,7 +56,7 @@ const Watch = () => {
                     <img className="h-full w-full object-cover" src="https://imgs.search.brave.com/u3FUfiEc_iDQip6_Xv-rOV15GBFN6hsH_zAqDeu9Xig/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvODE4/MDIxMTgvcGhvdG8v/Y2xvc2V1cC1vZi1p/bWctZm91bmRlci1h/bmQtY2VvLW1hcmst/bWNjb3JtYWNrLWxp/Z29uaWVyLXBhLTgt/MTQtMTk2NS5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9YUNj/ZExpNjE4b19qSjNC/WjN4VTZyZGN6T25n/NmxmVXZHZlhIUVRx/RC1YST0" alt="" />
                 </div>
                 <div className="w-[15vw]">
-                  <h6 className="text-lg font-semibold">Channel name name</h6>
+                  <h6 className="text-lg font-semibold">{videoData?.snippet?.channelTitle}</h6>
                   <p className="text-sm text-gray-400 font-semibold">
                     22.6k subscribers
                   </p>
@@ -72,7 +75,7 @@ const Watch = () => {
                   <span className="text-lg">
                     <AiOutlineLike />
                   </span>
-                  Like
+                   {videoData?.statistics?.likeCount}
                 </button>
                 <button className="px-4 py-2 rounded-full font-semibold bg-zinc-700 hover:bg-zinc-600 flex items-center gap-1">
                   <span className="text-lg">
