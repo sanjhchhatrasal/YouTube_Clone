4ac39b1dd8643f2327fdfcd0e3d1d6f5f021151c
sanjh chhatrasal <sanjhchhatrasal@gmail.com>
2025-01-24 16:04:52 +0530

    api

--- src/Constants/constants.jsx
+++ src/Constants/constants.jsx
@@ -1,5 +1,5 @@
 
 export const API_KEY = "AIzaSyDV5Gzg0Phmk6e30C5-s6pV-7-7TESgMP4"
 export const BASE_URL = "https://www.googleapis.com/youtube/v3"
-export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=1&regionCode=IN&key=${API_KEY}`
-export const SEARCH_SUGGESTION_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=40&regionCode=IN&key=${API_KEY}`
+export const SEARCH_SUGGESTION_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
